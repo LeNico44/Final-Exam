6223d2454c7203b6ab29d822859f13294d1cb68b
PaulLAGUES <paul.lagues@epsi.fr>
2017-12-08 12:24:52 +0100

    tests 100% coverage

--- spec/convert.spec.js
+++ spec/convert.spec.js
@@ -5,7 +5,10 @@ describe('Test des parametres de la fonction', function() {
     });
     
     it('les 3 parametres doivent etre du bon type', function() {
-		
+		expect(convertisseur(2, 2, 'EUR')).toBe(null);
+		expect(convertisseur('USD', 2, 2)).toBe(null);
+		expect(convertisseur('USD', null, 'KRW')).toBe(null);
+		expect(convertisseur(2, 2, 2)).toBe(null);
     });
 
 });
@@ -13,23 +16,36 @@ describe('Test des parametres de la fonction', function() {
 describe('Test resultats de la fonction', function() {
 	
 	it('doit convertir des USD dans les autres monnaies', function() {
+		expect(convertisseur('USD', 2, 'EUR')).not.toBe(null);
+		expect(convertisseur('USD', 2, 'NZD')).not.toBe(null);
+		expect(convertisseur('USD', 2, 'KRW')).not.toBe(null);
 		
     });
     
     it('doit convertir des EUR dans les autres monnaies', function() {
+    	expect(convertisseur('EUR', 2, 'USD')).not.toBe(null);
+		expect(convertisseur('EUR', 2, 'NZD')).not.toBe(null);
+		expect(convertisseur('EUR', 2, 'KRW')).not.toBe(null);
+    	
 		
-    });
+	});
 
     it('doit convertir des KRW dans les autres monnaies', function() {
-		
+    	expect(convertisseur('KRW', 2, 'EUR')).not.toBe(null);
+		expect(convertisseur('KRW', 2, 'NZD')).not.toBe(null);
+		expect(convertisseur('KRW', 2, 'USD')).not.toBe(null);
+    	
     });
 
     it('doit convertir des NZD dans les autres monnaies', function() {
+    	expect(convertisseur('NZD', 2, 'EUR')).not.toBe(null);
+		expect(convertisseur('NZD', 2, 'USD')).not.toBe(null);
+		expect(convertisseur('NZD', 2, 'KRW')).not.toBe(null);
 		
     });
 
     it('ne doit pas convertir des mauvais montants', function() {
-		
+		expect(convertisseur('NZD', -2, 'KRW')).toBe(null);
     });
 
 });
